6e858858e479edf4eec35e45983ec9cea9265662
Corkborg Dev <dev@corkborg.local>
2026-04-17 17:46:27 +0400

    fix: add height:100% to #app so canvas fills viewport

--- src/App.svelte
+++ src/App.svelte
@@ -58,6 +58,7 @@
 <style>
   :global(*, *::before, *::after) { box-sizing: border-box; margin: 0; padding: 0; }
   :global(body) { overflow: hidden; width: 100vw; height: 100vh; }
+  :global(#app) { width: 100%; height: 100%; }
   main {
     width: 100%;
     height: 100%;
